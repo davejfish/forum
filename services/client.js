265b2d3d980c40d859616b7d631dfd17952300ff
Hathaway-Shan <hathaway.shan@gmail.com>
2022-07-13 15:29:01 -0700

    setup supabase updated url and key in services

--- services/client.js
+++ services/client.js
@@ -1,5 +1,7 @@
-const SUPABASE_URL = 'https://nwxkvnsiwauieanvbiri.supabase.co';
+const SUPABASE_URL = 'https://naecyvnfmwcpxcyroblz.supabase.co';
 const SUPABASE_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzAwMzQzNCwiZXhwIjoxOTUyNTc5NDM0fQ.8XIsU0FANdaNeQnT-DojpTL-GTlTPZ4CYZDEetpFpWc';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hZWN5dm5mbXdjcHhjeXJvYmx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTY2MjM4MDQsImV4cCI6MTk3MjE5OTgwNH0.etgq6aLYRaVsRE4OXn1toSNWnUhRvFKalDJSmm9q4mk';
 
 export const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+//Shan added supabase hookups
