56e8e9e46161c29babce0985f4bceec992a0a25b
Dave Fisher <dave.j.fish@gmail.com>
2022-07-13 16:15:05 -0700

    user profile page added

--- services/client.js
+++ services/client.js
@@ -3,5 +3,3 @@ const SUPABASE_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hZWN5dm5mbXdjcHhjeXJvYmx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTY2MjM4MDQsImV4cCI6MTk3MjE5OTgwNH0.etgq6aLYRaVsRE4OXn1toSNWnUhRvFKalDJSmm9q4mk';
 
 export const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-//Shan added supabase hookups
